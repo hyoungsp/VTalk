4cb4f309d76adbe752062aaa36c2604a75196b1f
hyoungsun <hyoungsun@uchicago.edu>
2018-01-05 11:54:13 -0600

    image upload

--- VTalk/VTalk/SignupViewController.swift
+++ VTalk/VTalk/SignupViewController.swift
@@ -9,17 +9,17 @@
 import UIKit
 import Firebase
 
-class SignupViewController: UIViewController {
+class SignupViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var signup: UIButton!
     @IBOutlet weak var cancel: UIButton!
-    
-    let remoteConfig = RemoteConfig.remoteConfig()
-    var color: String?
-    
+
+    let remoteconfig = RemoteConfig.remoteConfig()
+    var color : String?
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,28 +29,75 @@ class SignupViewController: UIViewController {
             m.right.top.left.equalTo(self.view)
             m.height.equalTo(20)
         }
-        
-        color = remoteConfig["splash_background"].stringValue
+        color = remoteconfig["splash_background"].stringValue
         statusBar.backgroundColor = UIColor(hex: color!)
+        
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(imagePicker)))
+        
+        
         signup.backgroundColor = UIColor(hex: color!)
         cancel.backgroundColor = UIColor(hex: color!)
+        
+        signup.addTarget(self, action: #selector(signupEvent), for: .touchUpInside)
+        cancel.addTarget(self, action: #selector(cancelEvent), for: .touchUpInside)
+        
         // Do any additional setup after loading the view.
     }
-
+    
+    @objc func imagePicker() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+       
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func signupEvent(){
+        Auth.auth().createUser(withEmail: email.text!, password: password.text!) { (user, err) in
+            
+            let uid = user?.uid
+            let image = UIImageJPEGRepresentation(self.imageView.image!, 0.1)
+            
+            Storage.storage().reference().child("userImages").child(uid!).putData(image!, metadata: nil, completion: { (data, error) in
+                
+                let imageUrl = data?.downloadURL()?.absoluteString
+                
+                Database.database().reference().child("users").child(uid!).setValue(["name":self.name.text!, "profileImageUrl": imageUrl])
+            })
+            
+            
+        }
+        
+        
+    }
+    @objc func cancelEvent(){
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+
